0832e4362fe755f820480dbd58c6b1c1c97628dd
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2020-12-09 20:27:13 -0600

    FIX: AMP Hack Bash

--- amp-custom-hack.sh
+++ amp-custom-hack.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Estoy en:"
+pwd
+
 for filename in out/posts/*.amp.html
 do
+    echo "AMP Found ->" $filename
+
     sed -i "" "s/<style amp-custom><\/style>//g" $filename
 
     sed -i "" "s/<style>/<style amp-custom>/g" $filename
