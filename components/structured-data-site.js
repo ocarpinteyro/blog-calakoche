729789c4e75fc30b5b8b89b3699f117e261ad2e2
Omar Carpinteyro <omar.carpinteyro@gmail.com>
2020-12-14 12:51:28 -0600

    ADD: AMP Publisher Logo and sizes

--- components/structured-data-site.js
+++ components/structured-data-site.js
@@ -1,17 +1,24 @@
-import { SITE_TITLE, SITE_LOGO, SITE_DOMAIN, SITE_DESCRIPTION } from '../lib/constants'
+import {
+    SITE_TITLE,
+    SITE_LOGO_PUBLISHER,
+    SITE_LOGO_PUBLISHER_WIDTH,
+    SITE_LOGO_PUBLISHER_HEIGHT,
+    SITE_DOMAIN,
+    SITE_DESCRIPTION,
+} from "../lib/constants";
 
 export default function StructuredDataSite() {
-  return (
-    <>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `
+    return (
+        <>
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: `
           {
             "@context": "https://schema.org",
             "@id": "${SITE_DOMAIN}/#brand",
             "@type": "Brand",
-            "logo": "${SITE_LOGO}",
+            "logo": "${SITE_LOGO_PUBLISHER}",
             "name": "${SITE_TITLE}",
             "url": "${SITE_DOMAIN}",
             "sameAs": [
@@ -22,12 +29,12 @@ export default function StructuredDataSite() {
             ]
           }
           `,
-        }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: `
+                }}
+            />
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: `
           {
             "@context": "https://schema.org",
             "@type": "WebSite",
@@ -39,15 +46,17 @@ export default function StructuredDataSite() {
               "@id": "${SITE_DOMAIN}/#publisher", 
               "name": "${SITE_TITLE}", 
               "logo": { 
-                "@type": "ImageObject", 
-                "url": "${SITE_LOGO}" 
+                "@type": "ImageObject",
+                "url":"${SITE_LOGO_PUBLISHER}",
+                "height":${SITE_LOGO_PUBLISHER_HEIGHT},
+                "width":${SITE_LOGO_PUBLISHER_WIDTH}
               } 
             },
             "description": "${SITE_DESCRIPTION}"
           }
           `,
-        }}
-      />
-    </>
-  );
+                }}
+            />
+        </>
+    );
 }
